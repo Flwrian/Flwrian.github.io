387b5620d0a7349c51bf46875138662d12e53488
Flwrian <floflomontage@gmail.com>
2023-03-17 13:22:00 +0100

    updates

---
+++ js/particles.js
@@ -0,0 +1,11 @@
+var particles = Particles.init({
+    selector: '.background',
+    color: '#DA0463',
+    maxParticles: 150
+});
+
+window.onload = function () {
+    Particles.init({
+        selector:'.background'
+    });
+};
